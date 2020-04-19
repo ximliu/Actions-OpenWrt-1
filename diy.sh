5196c1f3fd5eed7d8a996c9506404c93646dbbf9
liwenjie119 <liwenjie119@126.com>
2020-04-19 22:27:20 +0800

    first upload for newifi3

--- diy.sh
+++ diy.sh
@@ -5,5 +5,10 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/myapp/luci-app-adguardhome
+git clone https://github.com/2512500960/zzu-minieap-openwrt package/myapp/zzu-minieap-openwrt
+git clone https://github.com/tty228/luci-app-serverchan package/myapp/luci-app-serverchan
+./scripts/feeds update -a
+./scripts/feeds install -a

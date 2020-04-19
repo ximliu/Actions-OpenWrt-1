14231fa66ba5152faa31bc78c296767f5a8c0225
liwenjie119 <liwenjie119@126.com>
2020-04-19 22:31:18 +0800

    fix ssrplus

--- diy.sh
+++ diy.sh
@@ -10,5 +10,7 @@
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/myapp/luci-app-adguardhome
 git clone https://github.com/2512500960/zzu-minieap-openwrt package/myapp/zzu-minieap-openwrt
 git clone https://github.com/tty228/luci-app-serverchan package/myapp/luci-app-serverchan
+sed -i 's#ntfs-3g#antfs-mount#g' package/lean/automount/Makefile
+sed -i 's_#src-git_src-git_g' ./feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
